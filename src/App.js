9bc1144d376828c6e20893fee70f2bd7b91936ca
A <avilavate@live.in>
2019-08-30 15:34:27 +0530

    Added util helpers

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import helpers from './helpers';
+
+const Star = () => {
+  const stars = 6;
+  return (
+    <div className="game">
+      <div className="body">
+        <div className="left">
+          {
+            helpers.helper1(stars).map(starId =>
+              <div key={starId} className="star" />
+            )
+          }
+
+        </div>
+        <div className="right"><button className="number">9</button>
+        </div>
+      </div>
+      <div className="timer">Time Remaining: 10</div>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
+      <Star></Star>
     </div>
   );
 }
---
+++ src/helpers.js
@@ -0,0 +1,13 @@
+const helpers = {
+    helper1: function(range){
+        let rangeArray = [];
+        for (let i = 0; i < range; i++) {
+            rangeArray.push(i);
+        }
+        console.log(rangeArray);
+        return rangeArray;
+    },
+   
+};
+
+export default helpers;
